3ff8e7168926d1d30502fe52db608a51dcb6be37
Rahulnb1806 <rahulnb1806@gmail.com>
2026-03-24 09:54:06 +0530

    Time: 7 ms (94.66%), Space: 46.8 MB (84.39%) - LeetHub

---
+++ 0707-design-linked-list/0707-design-linked-list.java
@@ -0,0 +1,116 @@
+class MyLinkedList {
+    class Node
+    {
+        int val;
+        Node next;
+        Node(int val)
+        {
+            this.val=val;
+            next=null;
+        }
+    }
+    Node head;
+    Node tail;
+    int cnt;
+    public MyLinkedList() {
+        cnt=0;
+        head=tail=null;
+    }
+    
+    public int get(int index) {
+    if(index<0||index>=cnt) return-1;
+        Node temp=head;
+        for(int i=0;i<index;i++)
+        {
+            temp=temp.next;
+        }
+        return temp.val;
+    }
+    
+    public void addAtHead(int val) {
+        Node nn=new Node(val);
+        if(head==null)
+        {
+            head=nn;
+            tail=nn;
+        }
+        else
+        {
+            nn.next=head;
+            head=nn;
+        }
+        cnt++;
+    }
+    
+    public void addAtTail(int val) {
+        Node nn=new Node(val);
+        if(head==null)
+        {
+            head=nn;
+            tail=nn;
+        }
+        else
+        {
+            tail.next=nn;
+            tail=nn;
+        }
+        cnt++;
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index<0||index>cnt) return;
+        Node nn=new Node(val);
+        Node temp=head;
+        if(index==0) 
+        {
+            addAtHead(val);
+            return;
+        }
+        if(index==cnt)
+        {
+            addAtTail(val);
+            return;
+        } 
+        for(int i=0;i<index-1;i++)
+        {
+            temp=temp.next;
+        }
+        nn.next=temp.next;
+        temp.next=nn;
+        cnt++;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(index<0||index>=cnt) return;
+        
+        if(index==0) 
+        {
+            head=head.next;
+            if(head==null)
+            {
+                tail=null;
+            }
+            cnt--;
+            return;
+        }
+            Node temp=head;
+            for(int i=0;i<index-1;i++)
+            {
+                temp=temp.next;
+            }
+            temp.next=temp.next.next;
+            cnt--;
+            if(temp.next==null) tail=temp;
+        
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
